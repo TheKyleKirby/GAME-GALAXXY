00e29205b9e2bc8b4dcb2b5c991a78e09f43b4c0
Tish <tishthegoat@gmail.com>
2024-08-02 20:11:58 -0600

    plan update, some typeDefs for queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -5,7 +5,22 @@ const typeDefs = `
 		username: String!
 		email: String!
 		password: String!
+	},
+
+
+
+	type Query {
+		allUsers: [users]
+		
+		searchedUsers(username: String!) : [Users]
+	}
+
+	type Mutation {
+	addUser(username: String!, email: String!, password: String!): Auth
+	login(username: String!, password: String!): Auth
 	}
 `
+
+// mutations
 
 module.exports = typeDefs
