7b8e6746b86eec0b2b69ab14e9b6a11fcdeecc5f
Reputation <kylekirby86@gmail.com>
2024-08-04 23:08:31 -0600

    Added Homepage, and Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
-import { Link, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import TestHome from './pages/TestHome'
 import FakeOther from './pages/FakeOther'
+import Navbar from './pages/Navbar'
+import Homepage from './pages/Homepage'
+import Footer from './pages/Footer'
 
 const App = () => {
     return (
         <>
             <Routes>
                 <Route path='/' element={<TestHome />} />
+                <Route path='/navbar' element={<Navbar />} />
+                <Route path='/footer' element={<Footer />} />
+                <Route path='/homepage' element={<Homepage />} />
                 {/* <Route path='/results' element={<ResultsPage />} /> */}
                 {/* <Route path='/createguide' element={<CreateGuidePage />} /> */}
                 {/* <Route path='/guide/:id' element={<GuidePage />} /> */}
