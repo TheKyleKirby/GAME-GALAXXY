221f7f42670dc07218e8df98645073613ea499a7
Reputation <kylekirby86@gmail.com>
2024-08-05 21:33:17 -0600

    Fixed duplicate on homepage and route to blog.

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
                             <a href="./Results" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white">Results</a>
                         </li>
                         <li>
-                            <a href="./Blog.jsx" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Blog</a>
+                            <a href="./Blog" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Blog</a>
                         </li>
                         <li>
                             <a href="./Tutorial" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Game Tutorials</a>
