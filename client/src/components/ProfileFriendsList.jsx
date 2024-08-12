706adfc817deffef9d0f33cfd16c1ef5a707e7a0
Tish <tishthegoat@gmail.com>
2024-08-12 05:13:26 -0600

    logged in profile working w/o friends

--- client/src/components/ProfileFriendsList.jsx
+++ client/src/components/ProfileFriendsList.jsx
@@ -1,40 +1,40 @@
-// import React from 'react'
-// import { useQuery } from '@apollo/client'
-// import { GET_USER_FRIENDS } from '../utils/queries'
-// import AuthService from '../utils/auth'
+// // import React from 'react'
+// // import { useQuery } from '@apollo/client'
+// // import { GET_USER_FRIENDS } from '../utils/queries'
+// // import AuthService from '../utils/auth'
 
-const ProfileFriendsList = () => {
+// const ProfileFriendsList = () => {
 
-	// todo function to find users by id's and map thru to make user cards or list names as links?/we could also just save usernames to an array.
+// 	// todo function to find users by id's and map thru to make user cards or list names as links?/we could also just save usernames to an array.
 
-// Get the logged-in user's ID from the decoded token
-	// const userId = AuthService.getProfile()?.id;
+// // Get the logged-in user's ID from the decoded token
+// 	// const userId = AuthService.getProfile()?.id;
 
-// using the logged in user's id to fetch data from users data to find it's array of friends
-	// const { data } = useQuery(GET_USER_FRIENDS, {
-	// 	variables: { userId },
-	// });
+// // using the logged in user's id to fetch data from users data to find it's array of friends
+// 	// const { data } = useQuery(GET_USER_FRIENDS, {
+// 	// 	variables: { userId },
+// 	// });
 
-	// const friends = data?.user?.friends || [];
+// 	// const friends = data?.user?.friends || [];
 
-	return (
-		<div className="bg-darkPurple-dark p-4 rounded-lg shadow-md w-full md:w-1/4 h-50 text-notWhite">
-			<h3 className="text-xl font-semibold mb-2 text-lightLavender-light">Friends List</h3>
-					<ul>
-					{friends.length > 0 ? (
-						friends.map(friend => (
-							<li key={friend.id}>
-								{/* <img src={friend.avatarUrl} alt={friend.name} /> */}
-								<p>{friend.name}</p>
-							</li>
-						))
-					) : (
-							<li>No friends found.</li>
-						)}
-					</ul>
-				</div>
-	)
-}
+// 	return (
+// 		<div className="bg-darkPurple-dark p-4 rounded-lg shadow-md w-full md:w-1/4 h-50 text-notWhite">
+// 			<h3 className="text-xl font-semibold mb-2 text-lightLavender-light">Friends List</h3>
+// 					<ul>
+// 					{friends.length > 0 ? (
+// 						friends.map(friend => (
+// 							<li key={friend.id}>
+// 								{/* <img src={friend.avatarUrl} alt={friend.name} /> */}
+// 								<p>{friend.name}</p>
+// 							</li>
+// 						))
+// 					) : (
+// 							<li>No friends found.</li>
+// 						)}
+// 					</ul>
+// 				</div>
+// 	)
+// }
 
-export default ProfileFriendsList
+// export default ProfileFriendsList
 
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,7 +1,7 @@
 import { useQuery } from '@apollo/client';
 import BioCard from '../components/BioCard';
 import ProfileFavoriteGames from '../components/ProfileFavoriteGames';
-import ProfileFriendsList from '../components/ProfileFriendsList';
+// import ProfileFriendsList from '../components/ProfileFriendsList';
 import ProfilePicture from '../components/ProfilePicture'
 import ProfileSavedTutorials from '../components/ProfileSavedTutorials';
 import { QUERY_USER, QUERY_ME } from '../utils/queries'
@@ -11,12 +11,21 @@ import Auth from '../utils/auth';
 
 const Profile = () => {
   
-  const { username: userParam } = useParams();
+  // const { username: userParam } = useParams();
 
-  const { loading, data, error } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
-    variables: { username: userParam },
-  });
+  // const { loading, data, error } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+  //   variables: { username: userParam },
+  // });
   
+const { data, loading, error } = useQuery(QUERY_ME, {
+  context:{
+    headers: {
+      authorization: `Bearer ${Auth.getToken}`
+    }
+  }
+})
+
+console.log(JSON.stringify(data))
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -29,9 +38,9 @@ const Profile = () => {
   const user = data?.me || data?.user || {};
 
     // navigate to personal profile page if username is yours
-  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
-    return <Navigate to="/profile" />;
-  }
+  // if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+  //   return <Navigate to="/profile" />;
+  // }
 
 
   if (!user?.username) {
@@ -49,21 +58,22 @@ const Profile = () => {
   // const games= data.games
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-deepBlue-dark pt-8 pb-16">
+    <div className="flex flex-col items-center gap-20 justify-center min-h-screen bg-deepBlue-dark pt-8 pb-16">
       <div className="fixed top-0 left-0 w-full bg-darkPurple-dark">
     </div>
-      <div className="flex items-center justify-center w-full mt-4">
+      <div className="flex items-center flex-col gap-20 justify-center w-full mt-4">
+        <h1 className='text-3xl text-goldenOrange-dark'>{user.username}</h1>
         {/* Profile Picture */}
         <ProfilePicture picture={user.profilePicture}/>
         </div>
         {/* Bio Card */}
         <BioCard bio={user.bio} />
       {/* Horizontal Cards */}
-      <div className="flex flex-wrap justify-center gap-4 w-full px-4 mt-8">
+      <div className="flex flex-wrap justify-center gap-10 w-full px-4 mt-8">
         
         <ProfileFavoriteGames /> {/* games={games} put in component to pass when we get game id's returning */} 
         
-        <ProfileFriendsList friends={user.friends}/> 
+        {/* <ProfileFriendsList friends={user.friends}/>  */}
 
         <ProfileSavedTutorials tutorials={user.savedTutorials} />
       
