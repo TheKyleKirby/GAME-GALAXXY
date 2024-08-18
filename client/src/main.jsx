38d7703d0a54dc6e4fb8212618c96246928fba85
Tish <tishthegoat@gmail.com>
2024-08-17 23:55:32 -0600

    changed server static back to 'dist' from 'client/dist' changed graphql uri to production site from localhost/graphql

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider, InMemoryCache, HttpLink, ApolloClient, ApolloLink } from '@apollo/client'
-// import { createUploadLink } from 'apollo-upload-client'
 import { setContext} from '@apollo/client/link/context'
 
 
 
 const httpLink = new HttpLink ({
-    uri: 'http://localhost:3001/graphql'
+    // changed to production site(requests were still coming from localhost.)
+    uri: 'https://game-galaxxy.onrender.com/'
 })
 
 const authLink = setContext((_, {headers} ) =>{
